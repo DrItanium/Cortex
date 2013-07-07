98d3ca1d25a8394ea18600176a7b776cd42ff548
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-07-07 00:31:42 -0700

    Added an assembly info file

---
+++ AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+[assembly:AssemblyVersionAttribute("1.0.0.0")]
+[assembly:AssemblyTitleAttribute("Cortex")]
+[assembly:AssemblyDescriptionAttribute("Cortex Extension Library")]
+[assembly:AssemblyCopyrightAttribute("\u2117 2010-2013 Joshua Scoggins")]
